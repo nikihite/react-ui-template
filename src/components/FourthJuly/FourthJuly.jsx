1d3dc288f0fb0ae5f797a9dfc8be945895924b7d
Niki Hite <nikihite@comcast.net>
2022-09-27 12:55:18 -0700

    added fourth of july page

--- src/components/FourthJuly/FourthJuly.jsx
+++ src/components/FourthJuly/FourthJuly.jsx
@@ -1,5 +1,13 @@
 export default function FourthJuly() {
   return (
-    <div>Fourth of July!!!</div>
+    <div>
+      <section>
+        <img src="/julyFourth.png"/>
+      </section>
+      <h1>LETS BLOW SHIT UP</h1>
+      <section>
+        <img src="/fireWorks.png"/>
+      </section>
+    </div>
   );
 }
